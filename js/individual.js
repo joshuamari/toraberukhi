bb7121862b11fadcfaccadd8293562404a82f52a
coquia-kdt <coquia-kdt@KDTW593>
2024-07-18 14:22:40 +0800

    ewan ko sayo dexml

--- js/individual.js
+++ js/individual.js
@@ -1,15 +1,5 @@
 //#region GLOBALS
-switch (document.location.hostname) {
-  case "kdt-ph":
-    rootFolder = "//kdt-ph/";
-    break;
-  case "localhost":
-    rootFolder = "//localhost/";
-    break;
-  default:
-    rootFolder = "//kdt-ph/";
-    break;
-}
+const rootFolder = document.location.hostname;
 let empDetails = [];
 var dispatch_days = 0;
 var to_add = 0;
